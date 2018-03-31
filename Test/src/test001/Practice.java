05407a4dad6d378d68a70021b867a33677419c5b
roger <roger@192.168.0.110>
2018-03-31 22:12:15 +0800

    add Practice Class

---
+++ Test/src/test001/Practice.java
@@ -0,0 +1,17 @@
+package test001;
+
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		System.out.println("Tom and Merry");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("please enter first member:");
+		String first = scanner.next();
+		System.out.println("please enter second member:");
+		String second = scanner.next();
+		scanner.close();
+		System.out.println(first + " and " +second);
+	}
+}
