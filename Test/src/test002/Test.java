b18839a2b9038e98db2ce583d618d6e93c77fe09
rogerggzhao <402100940@qq.com>
2018-04-04 17:03:23 +0800

    add：Test文件

---
+++ Test/src/test002/Test.java
@@ -0,0 +1,17 @@
+package test002;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println('2'+'2');
+		System.out.println("2"+"2");
+		char a = 65;
+		System.out.println(a);
+		char b = ' ';
+		System.out.println(b);
+		char c = 65535;
+		System.out.println(c);
+
+	}
+
+}
---
+++ Test/src/test003/Test.java
@@ -0,0 +1,9 @@
+package test003;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
