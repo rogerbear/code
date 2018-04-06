54dc4f39e07a85659f71afb68246ad43fe510f07
roger <roger@192.168.0.110>
2018-04-06 23:33:39 +0800

    add:文件

---
+++ Test/src/test004/Addition.java
@@ -0,0 +1,9 @@
+package test004;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Test/src/test004/ForDemo.java
@@ -0,0 +1,14 @@
+package test004;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for(int num = 1; num <= 100; num++) {
+			sum+=num;
+		}
+		System.out.println(sum);
+	}
+
+}
