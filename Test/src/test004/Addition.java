4860ba60ec00e591c5a884d0c275fe29ee56d489
roger <roger@192.168.0.110>
2018-04-07 09:14:45 +0800

    add:addition文件

--- Test/src/test004/Addition.java
+++ Test/src/test004/Addition.java
@@ -1,9 +1,29 @@
 package test004;
 
+import java.util.Scanner;
+
 public class Addition {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int score = 0;
+		for (int i = 1; i <= 10; i++) {
+			int a = (int) (Math.random() * 100);
+			int b = (int) (Math.random() * 100);
+			int result = a + b;
+			System.out.println("(" + i + ")" + a + "+" + b + "=");
+			int answer = scan.nextInt();
+			if (answer == -1) {
+				break;
+			}
+			if (answer == result) {
+				System.out.println("答对了");
+				score += 10;
+			} else {
+				System.out.println("答错了");
+			}
+		}
+		System.out.println("答对了" + score / 10 + "道题," + "共" + score + "分");
 	}
 
 }
